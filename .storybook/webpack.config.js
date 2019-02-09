5a568a208ccd23785344a87b3982e3cacbebbf7a
makoto tsuyuki <mtsuyuki@gmail.com>
2019-02-09 18:49:48 +0900

    don't use awesome-typescript-loader, change jsx to tst

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
 module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    use: [{
-      loader: require.resolve('awesome-typescript-loader')
-    }]
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [['react-app', { flow: false, typescript: true }]],
+    },
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
