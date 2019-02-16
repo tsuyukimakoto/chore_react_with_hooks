58abdb388da7bd0721071840f3014265d97ec75e
makoto tsuyuki <mtsuyuki@gmail.com>
2019-02-16 18:12:21 +0900

    add todo api

---
+++ src/api.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+
+export const api = axios.create({
+  baseURL: '/api',
+})
+
+export type TodoModel = {
+  id: string
+  headline: string
+  content: string
+  created_at: string
+}
+
+export const getTodos = () =>
+  api.get<TodoModel>('/todos?format=json')
+
+export const getTodo = (todoId: string) =>
+  api.get<TodoModel>(`/todos/${todoId}?format=json`)
