d2b0704949ce557327abbc2cb18e2941745e2367
makoto tsuyuki <mtsuyuki@gmail.com>
2019-02-23 12:45:18 +0900

    formated

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,25 @@ const App = () => {
         {id: 'a1', headline: 'headline1', content: 'this is content1', created_at: '2019-02-23 11:00'},
         {id: 'a2', headline: 'headline2', content: 'this is content2', created_at: '2019-02-23 12:00'},
         {id: 'a3', headline: 'headline3', content: 'this is content3', created_at: '2019-02-23 13:00'},
-        {id: 'a4', headline: 'headline4', content: 'this is content4', created_at: '2019-02-23 14:00'}
+        {id: 'a4', headline: 'headline4', content: 'this is content4', created_at: '2019-02-23 14:00'},
     ];
 
     const [ todos, setTodos ] = React.useState(todoData);
 
-    return <div>
-        <h1>Chore TODO</h1>
+    return (
         <div>
+            <h1>Chore TODO</h1>
             <div>
-                <h2>Add Todo</h2>
-            </div>
-            <div>
-                <h2>View Todos</h2>
-                <TodoTable />
+                <div>
+                    <h2>Add Todo</h2>
+                </div>
+                <div>
+                    <h2>View Todos</h2>
+                    <TodoTable />
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
 
 render(
