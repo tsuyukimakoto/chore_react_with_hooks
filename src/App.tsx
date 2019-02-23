b879ebcd5f8a4dd882da4f170a184e4888cf80c6
makoto tsuyuki <mtsuyuki@gmail.com>
2019-02-23 12:17:30 +0900

    initial html

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react'
-
 import { render } from 'react-dom'
 
 const App = () => (
-    <div />
+<div>
+    <h1>Chore TODO</h1>
+    <div>
+        <div>
+            <h2>Add Todo</h2>
+        </div>
+        <div>
+            <h2>View Todos</h2>
+        </div>
+    </div>
+</div>
 )
 
-render(<App />, document.getElementById('app'))
+render(
+  <App />,
+  document.getElementById('app') as HTMLElement
+);
