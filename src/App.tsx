abb2f21523d8b8281cb47b02c6b9402e78b0ab71
makoto tsuyuki <mtsuyuki@gmail.com>
2019-02-23 20:10:17 +0900

    implements upadteTodo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { render } from 'react-dom'
 import { TodoModel } from './api'
 import TodoTable from './components/notyetclassified/tables/TodoTable'
 import AddTodoForm from './components/notyetclassified/forms/AddTodoForm'
+import EditTodoForm from './components/notyetclassified/forms/EditTodoForm'
 
 const App = () => {
 
@@ -13,8 +14,11 @@ const App = () => {
         {id: 'a3', headline: 'headline3', content: 'this is content3', created_at: '2019-02-23 13:00'},
         {id: 'a4', headline: 'headline4', content: 'this is content4', created_at: '2019-02-23 14:00'},
     ];
+    const initialFormState:TodoModel = { id: null, headline: '', content: '' }
 
     const [ todos, setTodos ] = React.useState<TodoModel[]>(todoData);
+    const [ editing, setEditing ] = React.useState(false)
+    const [ currentTodo, setCurrentTodo ] = React.useState(initialFormState)
 
     const addTodo = (todo: TodoModel) => {
         todo.id = 'a' + (todos.length + 1)
@@ -25,18 +29,45 @@ const App = () => {
         setTodos(todos.filter(todo => todo.id !== id))
     }
 
+    const updateTodo = (id:string, updatedTodo:TodoModel) => {
+        setEditing(false)
+        setTodos(todos.map(todo => (todo.id === id ? updatedTodo : todo)))
+    }
+
+    const editRow = (todo:TodoModel) => {
+        setEditing(true)
+        setCurrentTodo({ id: todo.id, headline: todo.headline, content: todo.content })
+    }
 
     return (
         <div>
             <h1>Chore TODO</h1>
             <div>
-                <div>
-                    <h2>Add Todo</h2>
-                    <AddTodoForm addTodo={addTodo} />
-                </div>
+            {editing ?
+                (
+                    <div>
+                        <h2>Edit Todo</h2>
+                        <EditTodoForm
+                            editing={editing}
+                            setEditing={setEditing}
+                            currentTodo={currentTodo}
+                            updateTodo={updateTodo}
+                        />
+                    </div>
+                ) : (
+                    <div>
+                        <h2>Add Todo</h2>
+                        <AddTodoForm addTodo={addTodo} />
+                    </div>
+                )
+            }
                 <div>
                     <h2>View Todos</h2>
-                    <TodoTable todos={todos} deleteTodo={deleteTodo} />
+                    <TodoTable
+                        todos={todos}
+                        editRow={editRow}
+                        deleteTodo={deleteTodo}
+                    />
                 </div>
             </div>
         </div>
